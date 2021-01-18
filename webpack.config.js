d61838005dd794329f2959eddf23077f92cfc66a
Tom Niwinski <niwinski@yahoo.com>
2021-01-17 20:31:01 -0800

    production ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,42 @@
 const path = require("path");
-// entyr point -> output
-module.exports = {
-    entry: "./src/app.js",
-    output: { path: path.join(__dirname, "public"), filename: "bundle.js" },
-    module: {
-        rules: [
-            { loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ },
-            {
-                test: /\.s?css$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    devtool: "eval-cheap-module-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true,
-    },
-};
+const ExtractText = require("extract-text-webpack-plugin");
 
-//loader
+module.exports = (env, args) => {
+    const isProd = env === "production";
+    const CSSExtract = new ExtractText("styles.css");
+
+    return {
+        entry: "./src/app.js",
+        output: { path: path.join(__dirname, "public"), filename: "bundle.js" },
+        module: {
+            rules: [
+                {
+                    loader: "babel-loader",
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [
+                            {
+                                loader: "css-loader",
+                                options: { sourceMap: true },
+                            },
+                            {
+                                loader: "sass-loader",
+                                options: { sourceMap: true },
+                            },
+                        ],
+                    }),
+                },
+            ],
+        },
+        plugins: [CSSExtract],
+        devtool: isProd ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true,
+        },
+    };
+};
